f6d22505f4460b6d082c9fef7587bf2b0ad7e20e
gihani571 <gihanimadhubashini75@gmail.com>
2025-02-24 09:34:36 +0530

    first commit

---
+++ src/main/java/edu/icet/controller/StudentController.java
@@ -0,0 +1,16 @@
+package edu.icet.controller;
+
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+
+
+public class StudentController {
+
+
+}
---
+++ src/main/java/edu/icet/dtro/Student.java
@@ -0,0 +1,5 @@
+package edu.icet.dtro;
+
+public class Student {
+
+}
